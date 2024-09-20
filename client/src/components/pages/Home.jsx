89595b47c7ebc825b2e87923948f6d3597fd3e05
AYMAR ANTONIO DA SILVA <aymar.silva@labcetec.net>
2024-09-20 17:11:43 -0300

    adicionando template

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,11 +1,14 @@
+import LateralImagens from '../LateralImagens/LateralImagens';
 import Cabecalho from '../layout/Cabecalho';
 import TabelaAulas from '../tabelaAulas/TabelaAulas';
+
 
 export default function Home() {
   return (
     <>
       <Cabecalho />
       <TabelaAulas />
+      <LateralImagens />
     </>
   );
 };
